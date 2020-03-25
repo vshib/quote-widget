d24b9d5716f6d4d46364aa4196b0630b3522e07c
vshib <vlad.shibnev@gmail.com>
2020-03-26 02:35:12 +0300

    add fontello, icons, reload button

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   let currentQuote = ''
 
   let quotes = [
-    'Quote 1',
+    'Imagination is the one weapon in the war against reality.',
     'Some quote 1',
     'Quote 2',
     'Some quote 2',
@@ -13,10 +13,6 @@
     'Some quote 5',
     'Some quote 6',
   ]
-
-  function hover() {
-    console.log('hover')
-  }
 
   function nextQuote() {
     currentQuote = getQuote()
@@ -38,15 +34,70 @@
 </script>
 
 <style>
+  :global(body) {
+    background: #2b2a29;
+  }
   .window {
     width: 320px;
     height: 150px;
     background: white;
     color: black;
+    border: 1px solid black;
+    font-size: 17px;
+    border-radius: 4px;
+    cursor: pointer;
+
+    justify-content: center;
+    align-items: center;
+
+    flex-wrap: wrap;
+    display: flex;
   }
-  .window:hover {
-    background: yellow;
+
+  .quote-text {
+    padding: 0 35px;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+  }
+  .icon-quote-left {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+    padding-top: 15px;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+  .icon-quote-right {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    align-items: center;
+    padding-bottom: 15px;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+
+  .icon-arrows-ccw {
+    color: #04a49c;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-bottom: 15px;
+    align-items: center;
   }
 </style>
 
-<div class="window" on:mouseover={hover} on:click={nextQuote}>{currentQuote}</div>
+<div class="window">
+
+  <div class="icon-quote-left" />
+  <div class="quote-text">{currentQuote}</div>
+  <div class="icon-quote-right" />
+
+  <div on:click={nextQuote} class="icon-arrows-ccw" />
+
+</div>
