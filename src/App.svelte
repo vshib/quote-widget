f9c13198dfacd4a78ac1e8c091195880279b6fb3
vshib <56637277+vshib@users.noreply.github.com>
2020-03-25 22:48:04 +0300

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Spinner from 'svelte-spinner'
+</script>
+
+<style>
+
+</style>
+
+<Spinner size="50" speed="750" color="#A82124" thickness="2" gap="40" />
+
+ <div class="box widget_tag_cloud" data-role="widget_get_new_quote"> ==$0
+ 
+ </div>
